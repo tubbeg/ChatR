f09c7b3fded1521511a89ad7482dd2262e31726c
Tobias Gärdebro <tubbeg@gmail.com>
2020-07-30 20:08:30 +0200

    Added helper functions in separate ts-file for client. Also added EF Core for data persistancy.

--- ChatR/Hubs/TextHub.cs
+++ ChatR/Hubs/TextHub.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +10,35 @@ namespace ChatR.Hubs
 {
     public class TextHub : Hub
     {
-        public async Task NewMessage(long username, string message)
+        private readonly IServiceScopeFactory _scopeFactory;
+        public TextHub(IServiceScopeFactory scopeFactory)
         {
-            await Clients.All.SendAsync("messageReceived", username, message);
+            _scopeFactory = scopeFactory;
         }
+        public async Task NewMessage(IMessage message)
+        {
+            await Clients.All.SendAsync("messageReceived", message.Author, message.Content);
+        }
+
+
+
+        public void SaveMessageAsRecord()
+        {
+            using (var scope = _scopeFactory.CreateScope())
+            {
+                var db = scope.ServiceProvider.GetRequiredService<DbContext>();
+
+                // when we exit the using block,
+                // the IServiceScope will dispose itself 
+                // and dispose all of the services that it resolved.
+            }
+        }
+    }
+
+    public interface IMessage
+    {
+        public string Author { get; set; }
+        public string Content { get; set; }
+        public string Type { get; set; }
     }
 }
--- ChatR/scripts/index.ts
+++ ChatR/scripts/index.ts
@@ -1,4 +1,5 @@
 ﻿import * as signalR from "@microsoft/signalr";
+import { validURL, checkImage } from "./checkString";
 
 const divMessages: HTMLDivElement = document.querySelector("#divMessages");
 const tbMessage: HTMLInputElement = document.querySelector("#tbMessage");
@@ -9,11 +10,17 @@ const connection = new signalR.HubConnectionBuilder()
     .withUrl("/hub")
     .build();
 
-connection.on("messageReceived", (username: string, message: string) => {
+interface Message {
+    author: string,
+    content: string,
+    type: string,
+}
+
+connection.on("messageReceived", (message : Message) => {
     let messages = document.createElement("div");
 
     messages.innerHTML =
-        `<div class="message-author">${username}</div><div>${message}</div>`;
+        `<div class="message-author">${message.author}</div><div>${message.content}</div>`;
 
     divMessages.appendChild(messages);
     divMessages.scrollTop = divMessages.scrollHeight;
@@ -30,6 +37,8 @@ tbMessage.addEventListener("keyup", (e: KeyboardEvent) => {
 btnSend.addEventListener("click", send);
 
 function send() {
-    connection.send("newMessage", username, tbMessage.value)
+    let message: Message = { author: "", content: tbMessage.value, type: "" }
+    connection.send("newMessage", message)
         .then(() => tbMessage.value = "");
 }
+
