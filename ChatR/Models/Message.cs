e696a14083746893ccfe0b05b302da771e7abc5b
Tobias Gärdebro <tubbeg@gmail.com>
2020-08-10 07:51:27 +0200

    Migrated DB and scaffolded api controller.

--- ChatR/Models/Message.cs
+++ ChatR/Models/Message.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +9,8 @@ namespace ChatR.Models
 
     public class Message : IMessage
     {
+        [Key]
+        public int Key { get; set; }
         public string Author { get; set; }
         public string Content { get; set; }
         public MessageType Type { get; set; }
