92c27c4bb79b45b5adefc8e379badb7609376329
Tobias Gärdebro <tubbeg@gmail.com>
2020-08-25 21:56:34 +0200

    Debugged hub. So far it works as intended. Client user could see user history. Need a way to test different users. Opening a new tab will generate the same UserIdentifier which is not perfect.

---
+++ ChatR/Migrations/20200825194918_Refactor.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ChatR.Migrations
+{
+    public partial class Refactor : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Username",
+                table: "Users",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserIdentifier",
+                table: "Users",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_UserIdentifier_Username",
+                table: "Users",
+                columns: new[] { "UserIdentifier", "Username" },
+                unique: true,
+                filter: "[UserIdentifier] IS NOT NULL AND [Username] IS NOT NULL");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Users_UserIdentifier_Username",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "UserIdentifier",
+                table: "Users");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Username",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+    }
+}
